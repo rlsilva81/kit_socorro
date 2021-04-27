ee2c074fc9f4b39758757dfec6037545acda3bc1
rlsilva81 <romulo.silva@restinga.ifrs.edu.br>
2021-04-27 11:46:01 -0300

    apagadas linhas em branco e inserida msg Bom tarde

--- src/application/program.java
+++ src/application/program.java
@@ -3,8 +3,7 @@ package application;
 public class program {
 
 	public static void main(String[] args) {
-		System.out.println("Bom dia");
-
+		System.out.println("Bom dia!");
+		System.out.println("Boa tarde!");
 	}
-
 }
